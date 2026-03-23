004bba9ec568873f6ff0c5f19ab1914bb09980fd
xyfuss <jonasaatomren@gmail.com>
2026-03-23 10:25:50 +0100

    add HTTP server setup and notification route handlers (TEMPORARY)

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"assignment_2/internal/handlers"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("working")
+	mux := http.NewServeMux()
+
+	// routes
+	mux.HandleFunc("POST /notifications/", handlers.CreateNotificationHandler)
+	mux.HandleFunc("GET /notifications/{id}", handlers.GetNotificationHandler)
+	mux.HandleFunc("GET /notifications/", handlers.ListNotificationsHandler)
+	mux.HandleFunc("DELETE /notifications/{id}", handlers.DeleteNotificationHandler)
+
+	log.Println("Server running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
