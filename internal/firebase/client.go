32d3ccb3321627548b5eb145541a613c4d5eac7e
Stankel <sindresmedalolsen@gmail.com>
2026-04-07 14:51:43 +0200

    firebase/client.go implemented and firebase package installed

--- internal/firebase/client.go
+++ internal/firebase/client.go
@@ -1,1 +1,28 @@
 package firebase
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go/v4"
+	"google.golang.org/api/option"
+)
+
+// NewFirestoreClient initialises a Firebase app and returns a Firestore client
+func NewFirestoreClient(ctx context.Context) (*firestore.Client, error) {
+	opt := option.WithCredentialsFile(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+
+	app, err := firebase.NewApp(ctx, nil, opt)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialise Firebase app: %w", err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialise Firestore client: %w", err)
+	}
+
+	return client, nil
+}
