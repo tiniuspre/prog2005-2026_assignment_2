84cc446510bf14c0ffab3f1242763e04b7ed8b69
Stankel <sindresmedalolsen@gmail.com>
2026-04-09 14:15:45 +0200

    removed redundant model

--- internal/models/exchange.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type ExchangeResponse struct {
-	Country        string             `json:"country"`
-	BaseCurrency   string             `json:"base-currency"`
-	TargetCurrencies map[string]float64 `json:"targetCurrencies"`
-}
