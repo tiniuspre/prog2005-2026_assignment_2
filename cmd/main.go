a16cbc2642f8a5625104bcd598346544e7d23a5b
xyfuss <jonasaatomren@gmail.com>
2026-04-10 02:07:14 +0200

    add routes for dashboards, registrations etc.

--- cmd/main.go
+++ cmd/main.go
@@ -9,11 +9,21 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	// routes
-	mux.HandleFunc("POST /notifications/", handlers.CreateNotificationHandler)
-	mux.HandleFunc("GET /notifications/{id}", handlers.GetNotificationHandler)
-	mux.HandleFunc("GET /notifications/", handlers.ListNotificationsHandler)
-	mux.HandleFunc("DELETE /notifications/{id}", handlers.DeleteNotificationHandler)
+	// registration routes
+	mux.HandleFunc("POST /envdash/v1/registrations/", handlers.CreateRegistrationHandler)
+	mux.HandleFunc("GET /envdash/v1/registrations/{id}", handlers.GetRegistrationHandler)
+	mux.HandleFunc("GET /envdash/v1/registrations/", handlers.ListRegistrationsHandler)
+	mux.HandleFunc("PUT /envdash/v1/registrations/{id}", handlers.UpdateRegistrationHandler)
+	mux.HandleFunc("DELETE /envdash/v1/registrations/{id}", handlers.DeleteRegistrationHandler)
+
+	// dashboard routes
+	mux.HandleFunc("GET /envdash/v1/dashboards/{id}", handlers.GetDashboardHandler)
+
+	// notification routes
+	mux.HandleFunc("POST /envdash/v1/notifications/", handlers.CreateNotificationHandler)
+	mux.HandleFunc("GET /envdash/v1/notifications/{id}", handlers.GetNotificationHandler)
+	mux.HandleFunc("GET /envdash/v1/notifications/", handlers.ListNotificationsHandler)
+	mux.HandleFunc("DELETE /envdash/v1/notifications/{id}", handlers.DeleteNotificationHandler)
 
 	log.Println("Server running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
